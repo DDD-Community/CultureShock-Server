9b43933d95e2421a3044b9dd8ccbb7f61cb9f481
ChoiPuReum <pooreumsunny@gmail.com>
2021-11-10 00:01:30 +0900

    Add ticket controller
    Signed-off-by: ChoiPuReum <pooreumsunny@gmail.com>

---
+++ src/main/kotlin/com/cultureshock/madeleine/rest/controller/TicketController.kt
@@ -0,0 +1,38 @@
+package com.cultureshock.madeleine.rest.controller
+
+import com.cultureshock.madeleine.rest.RestSupport
+import com.cultureshock.madeleine.rest.dto.request.TicketMyListRequest
+import com.cultureshock.madeleine.service.performance.PerformanceService
+import com.cultureshock.madeleine.service.ticket.TicketService
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Pageable
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@Api(tags = ["티켓"])
+@RestController
+@RequestMapping("/api/v1/ticket")
+class TicketController(
+    private var ticketService: TicketService
+): RestSupport() {
+
+    @PostMapping("/my/list", produces = ["application/json"])
+    @ApiOperation(value="내 티켓 전체 리스트 조회", notes = "내 티켓 전체 리스트 조회")
+    fun getMyTicketList(
+        @RequestBody ticketMyListRequest: TicketMyListRequest?
+    ): ResponseEntity<Any> {
+        val paging: Pageable = PageRequest.of(ticketMyListRequest?.page?: 0,ticketMyListRequest?.size?: 10)
+        return response(ticketService.findAllByUserId(ticketMyListRequest?.userId?: 0,  paging))
+    }
+
+
+    @GetMapping("/{ticketId}", produces = ["application/json"])
+    @ApiOperation(value="티켓 상세 조회", notes = "티켓 상세 조회 by 티켓 ID")
+    fun getByTicketId(
+        @PathVariable ticketId: Long
+    ): ResponseEntity<Any> {
+        return response(ticketService.findByTicketId(ticketId))
+    }
+}
--- src/main/kotlin/com/cultureshock/madeleine/rest/dto/request/TicketMyListRequest.kt
+++ src/main/kotlin/com/cultureshock/madeleine/rest/dto/request/TicketMyListRequest.kt
@@ -3,12 +3,10 @@ package com.cultureshock.madeleine.rest.dto.request
 import com.fasterxml.jackson.databind.PropertyNamingStrategy
 import com.fasterxml.jackson.databind.annotation.JsonNaming
 
-
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy::class)
-data class PerformanceListRequest(
-    val kind: Int? = 0,
-    val state: Int? = 0,
-    val location: Int? = 0,
+data class TicketMyListRequest(
+    val userId: Long = 0,
     val size: Int? = 10,
     val page: Int? = 0
 )
+
